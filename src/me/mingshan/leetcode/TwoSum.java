d353d053c68169798166618217ccccdb1e2d89bf
Juntao Han <499445428@qq.com>
2018-09-15 16:42:22 +0800

    Initial commit.

---
+++ src/me/mingshan/leetcode/TwoSum.java
@@ -0,0 +1,71 @@
+package me.mingshan.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 描述：
+ *
+ *  给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+ *  你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ */
+public class TwoSum {
+
+    public static void main( String[] args ) {
+        TwoSum t = new TwoSum();
+        int[] nums = {5,4, 2};
+        int target = 6;
+        int[] result = t.twoSum2(nums, target);
+        for (int i = 0; i< result.length; i++ ) {
+            System.out.println(result[i]);
+        }
+    }
+
+    /**
+     *
+     * 暴力方式，直接遍历数组，找出与nums[i]相加为target的元素，然后返回下标。
+     * 因为要循环两次，时间复杂度为O(n * n)
+     * 空间复杂度为O(1)。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 利用一个map将遍历过的数组的元素存起来，然后通过map来判断target - nums[i]元素是否存在，
+     * 只需遍历一次即可，所以时间复杂度为O(n)；
+     * 在最坏的情况下，需要map需要存储数组的所有元素，所以空间复杂度为O(n)。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> a = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (a.containsKey(x)) {
+                return new int[] {a.get(x), i};
+            }
+
+            a.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+     }
+}
