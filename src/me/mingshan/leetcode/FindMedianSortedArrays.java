31e264309c7db104fb61bdb2190e8644a08fede3
Juntao Han <499445428@qq.com>
2018-10-11 08:50:31 +0800

    Update FindMedianSortedArrays.java

--- src/me/mingshan/leetcode/FindMedianSortedArrays.java
+++ src/me/mingshan/leetcode/FindMedianSortedArrays.java
@@ -1,6 +1,14 @@
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
+/**
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+ * 请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+ * 你可以假设 nums1 和 nums2 不同时为空。
+ * 
+ * @author mingshan
+ *
+ */
 public class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         DecimalFormat df = new DecimalFormat("0.0");
